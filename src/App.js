4a1e2bb9a3e311ff4f4bd594ef5940d4de1b40c8
Bug520-Maker <betweent@126.com>
2021-01-24 11:18:36 +0800

    完善CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ class App extends PureComponent {
     render() {
         return (
             <div>
-
+                <input type="text"/>
+                <button>34</button>
+                <ul>
+                    <li></li>
+                </ul>
+                <a href="https://www.baidu.com">213</a>
             </div>
         );
     }
